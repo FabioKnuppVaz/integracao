86f413f1cc8d84c630cefc2a8c3049dd8ee8b9db
fabio <fabio.knupp@gmail.com>
2023-01-20 17:24:25 -0300

    melhoria de properties

--- src/main/java/app/services/InpeServices.java
+++ src/main/java/app/services/InpeServices.java
@@ -1,5 +1,6 @@
 package app.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,30 +11,33 @@ import app.pojos.Cidades;
 @Service
 public class InpeServices {
 
-	private final RestTemplate restTemplate;
+	@Value("${inpe.url}")
+	private String inpeUrl;
+	
+	private RestTemplate restTemplate;
 
     public InpeServices(RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplate = restTemplateBuilder.build();
+        restTemplate = restTemplateBuilder.build();
     }
 
     public Cidades getLocalidade(String localidade) {
-        String url = "http://servicos.cptec.inpe.br/XML/listaCidades?city=" + localidade;
-        return this.restTemplate.getForObject(url, Cidades.class);
+        String url = inpeUrl + "/listaCidades?city=" + localidade;
+        return restTemplate.getForObject(url, Cidades.class);
     }
 
     public Cidade getPrevisao4Dias(String id) {
-        String url = "http://servicos.cptec.inpe.br/XML/cidade/" + id + "/previsao.xml";
-        return this.restTemplate.getForObject(url, Cidade.class);    	
+        String url = inpeUrl + "/cidade/" + id + "/previsao.xml";
+        return restTemplate.getForObject(url, Cidade.class);    	
     }
     
     public Cidade getPrevisao7Dias(String id) {
-        String url = "http://servicos.cptec.inpe.br/XML/cidade/7dias/" + id + "/previsao.xml";
-        return this.restTemplate.getForObject(url, Cidade.class);    	
+        String url = inpeUrl + "/cidade/7dias/" + id + "/previsao.xml";
+        return restTemplate.getForObject(url, Cidade.class);    	
     }
 
 	public Cidade getPrevisaoExtendida(String id) {
-		String url = "http://servicos.cptec.inpe.br/XML/cidade/" + id + "/estendida.xml";
-        return this.restTemplate.getForObject(url, Cidade.class); 
+		String url = inpeUrl + "/cidade/" + id + "/estendida.xml";
+        return restTemplate.getForObject(url, Cidade.class); 
 	}
 
 
